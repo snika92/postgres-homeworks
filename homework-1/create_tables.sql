7346a82134def141fb4432d488a5981f4f4d5dc4
Veronika Surkova <surkova_nika92@mail.ru>
2023-10-06 14:06:44 +0300

    add create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,32 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(15) NOT NULL,
+	last_name varchar(15) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date NOT NULL,
+	notes varchar(100) NOT NULL
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(15) NOT NULL,
+	contact_name varchar(15) NOT NULL
+);
+
+SELECT * FROM customers;
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(15) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(25) NOT NULL
+);
+
+SELECT * FROM orders;
